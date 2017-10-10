94d37a08e172ea06b299751cc55bd8a0d2bc7d85
Nathan N <Nitharios@users.noreply.github.com>
2017-10-09 20:19:56 -1000

    Refactored and able to change data using PUT for products

--- db/products.js
+++ db/products.js
@@ -1,35 +1,59 @@
 /* jshint esversion:6 */
-const validReq = { "success" : true };
-const invalidReq = { "success" : false };
-
-let productId = {};
-let productNumber = 0;
+// let productList = {};
+// let productNumber = 0;
 
 class Products {
   constructor() {
+    this._productList = {};
+    this._productNumber = 0;
   }
-  // will verify that the req body is a valid format
-  verify(req) {
-
+  // will return full productlist
+  listAll () {
+    return this._productList;
   }
   // will save data from req body
-  create(req) {
+  create(data) {
 
     let productInfo = {
-      name : req.body.name,
-      price : Number(req.body.price),
-      inventory : Number(req.body.inventory)
+      name : data.name,
+      price : Number(data.price),
+      inventory : Number(data.inventory)
     };
 
-    productNumber++;
-    productId[productNumber] = productInfo;
+    for (let i in this._productList) {
+      if (this._productList[i].name === productInfo.name) return false;
+    }
+
+    this._productNumber += 1;
+    this._productList[this._productNumber] = productInfo;
+    console.log(this._productList);
+    return true;
+  }
+
+  // will return reference to location of id through coersion
+  find(id) {
+    if (this._productList.hasOwnProperty(id)) return this._productList[id];
   }
 
   // will edit a product based on id
-  edit(req) {
+  edit(data, newInfo) {
+    if (data.name) newInfo.name = data.name;
+    if (data.price) newInfo.price = data.price;
+    if (data.inventory) newInfo.inventory = data.inventory;
 
-
+    return newInfo;
   }
 } 
 
 module.exports = Products;
+
+  // will verify that the req body is a valid format
+  // verify(data) {
+  //   try {
+  //     return this.create(data);
+    
+  //   } catch (e) {
+  //     console.log(e);
+  //     return false;
+  //   }
+  // }
--- routes/products.js
+++ routes/products.js
@@ -6,6 +6,9 @@ const hbs = require('express-handlebars');
 const Products = require('../db/products');
 const products = new Products();
 
+const validReq = { "success" : true };
+const invalidReq = { "success" : false };
+
 router.route('/')
   .get((req, res) => {
   res.render(/*../views/products.hbs*/);
@@ -13,7 +16,9 @@ router.route('/')
 
   .post((req, res) => {
     console.log(req.body);
-    products.create(req);
+    
+    if (products.create(req.body)) return res.json(validReq);
+    else return res.json(invalidReq);
   });
 
 router.route('/:id')
@@ -22,11 +27,15 @@ router.route('/:id')
   })
 
   .put((req, res) => {
+    let id = req.url.split('/')[1];
+    let newInfo = products.find(id);
 
+    products.edit(req.body, newInfo);
+    // console.log(products.listAll());
   })
 
   .delete((req, res) => {
-    
-  })
+
+  });
 
 module.exports = router;
