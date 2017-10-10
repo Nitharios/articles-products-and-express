7e2019e6832c8ec5986c670a48e3d71bd9b9a6ac
Nathan N <Nitharios@users.noreply.github.com>
2017-10-09 14:42:01 -1000

    Setting up

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ console.log(sanity);
 
 const express = require('express');
 const hbs = require('express-handlebars');
+const path = require('path');
+const fs = require('fs');
+
+const products = require('./routes/products');
 
 const port = process.env.PORT || 8888;
 
@@ -12,6 +16,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.get('/products', products);
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
