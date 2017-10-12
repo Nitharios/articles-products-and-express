bee80cdf7543c68cc515b37b8e8dcd4ec9938dd5
Nathan N <Nitharios@users.noreply.github.com>
2017-10-11 20:00:36 -1000

    Edited more code

--- db/products.js
+++ db/products.js
@@ -1,79 +1,104 @@
 /* jshint esversion:6 */
-// let productList = {};
-// let productNumber = 0;
 
 class Products {
   constructor() {
-    this._productList = {
-      '1' : {
+    this._productList = [
+      {
         'id' : '1',
         'name' : 'potato chips',
         'price' : 2,
         'inventory' : 20
       },
 
-      '2' : {
+      {
         'id' : '2',
         'name' : 'watermelon',
         'price' : 10,
         'inventory' : 5
       }
-    };
+    ];
+
     this._productNumber = 2;
   }
   // will return full product list
   listAll () {
     return this._productList;
   }
-  // will save data from req.body
+  // will save data from req.name
   create(data) {
-    if (this._productList.hasOwnProperty(data.name)) return false;
+    if (this.verify(data.id)) return false;
 
     this._productNumber += 1;
 
     let productInfo = {
-      id : this._productNumber,
+      id : data.id,
       name : data.name,
       price : Number(data.price),
       inventory : Number(data.inventory)
     };
 
-    this._productList[this._productNumber] = productInfo;
-    console.log(this._productList);
+    this._productList.push(productInfo);
+    console.log('productList', this._productList);
     return true;
   }
 
   // will return reference to location of id through coersion
-  find(id) {
-    if (this._productList.hasOwnProperty(id)) return this._productList[id];
-    else return false;
+  verify(id) {
+    return this._productList.some(element => {
+      return element.id === id;
+    })
+
+    return false;
+  }
+
+  locate(id) {
+    return this._productList.findIndex((element, index) => {
+      return element.id === id;
+    })
+  }
+
+  retrieve(id) {
+    return this._productList.find(element => {
+      return element.id === id;
+    })
+
+    return false;
   }
 
   // will edit a product based on id
-  edit(data, item) {
-    if (data.name) item.name = data.name;
-    if (data.price) item.price = data.price;
-    if (data.inventory) item.inventory = data.inventory;
+  edit(data) {
+    if (this.verify(data.id)) {
+      let index = this.locate(data.id);
+      let targetItem = this._productList[index];
 
-    return true;
+      if (data.id) targetItem.id = data.id;
+      if (data.name) targetItem.name = data.name;
+      if (data.price) targetItem.price = data.price;
+      if (data.inventory) targetItem.inventory = data.inventory;
+      
+      return true;
+    }
+
+    return false;
   }
 
   // will delete a product based on id
   remove(id) {
-    if (this._productList.hasOwnProperty(id)) {
-      this._productList[id] = {};
-      return true;
-
-    } else {
-     
-      return false;
+    console.log(id);
+    if (this.verify(id)) {
+      return this._productList.filter((element) => {
+        console.log('here', element);
+        return element.id !== id;
+      })
     }
+
+    return false;
   }
 } 
 
 module.exports = Products;
 
-  // will verify that the req body is a valid format
+  // will verify that the req name is a valid format
   // verify(data) {
   //   try {
   //     return this.create(data);
