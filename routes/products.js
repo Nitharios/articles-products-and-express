181c953a10154bbf0d5522c1fc4cb53796e14b9b
Nathan N <Nitharios@users.noreply.github.com>
2017-10-16 16:59:48 -1000

    Cleaning up code

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,9 @@
 /* jshint esversion:6 */
 const express = require('express');
-const router = express.Router();
-
 const Products = require('../models/products');
+
 const products = new Products();
+const router = express.Router();
 
 // handles returning entire table of products
 router.route('/')
