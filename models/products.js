c2acad5838520916d05e4bb35bf093b6d55f3960
Nathan N <Nitharios@users.noreply.github.com>
2017-10-16 14:02:48 -1000

    More routes

--- models/products.js
+++ models/products.js
@@ -1,6 +1,7 @@
 /* jshint esversion:6 */
 const pgp = require('pg-promise')();
 const port = 5432;
+// this data can go in another file and then be required in since Products AND Articles will use this
 const database = 'articles_and_products';
 const user = 'db_manager';
 const connect = {
--- routes/products.js
+++ routes/products.js
@@ -6,12 +6,23 @@ const Products = require('../models/products');
 const products = new Products();
 
 router.route('/')
-  .get((req, res) => {
-    // listAll will return a Promise
-    return products.listAll()
-    .then((data) => {
-      res.json(data);
-    });
+.get((req, res) => {
+  // listAll will return a Promise
+  return products.listAll()
+  .then((data) => {
+    res.json(data);
   });
+});
+
+router.route('/new')
+.get((req, res) => {
+  res.render('index');
+})
+.post((req, res) => {
+  return products.create(req.body)
+  .then((data) => {
+    res.json(data);
+  });
+});
 
 module.exports = router;
